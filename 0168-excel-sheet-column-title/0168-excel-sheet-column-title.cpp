f080dd18657321474441833c99cdd78d5c8adc58
influx <71868156+Abhi-187@users.noreply.github.com>
2023-08-22 12:13:16 +0530

    Update 0168-excel-sheet-column-title.cpp

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string convertToTitle(int n) {
-        string result;
-        while(n>0){
-            n--;
-            int rem = n%26;
-            char ch = rem + 'A';
-            result.push_back(ch);
-            n/=26;
+        string result;  // This string will store the Excel column title
+        while (n > 0) {
+            n--;  // Decrement n by 1 to map it to 0-based index
+            int rem = n % 26;  // Calculate the remainder when dividing n by 26
+            char ch = rem + 'A';  // Convert the remainder to a character using ASCII offset ('A' is 65)
+            result.push_back(ch);  // Add the character to the result string
+            n /= 26;  // Divide n by 26 to move to the next position in the title
         }
-        reverse(begin(result),end(result));
-        return result;
+        reverse(begin(result), end(result));  // Reverse the result string to get the correct order
+        return result;  // Return the final Excel column title
     }
 };
