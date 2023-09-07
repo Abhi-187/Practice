3e9c99d9a750261c068b8b9b1b8e5a4141fc5866
influx <71868156+Abhi-187@users.noreply.github.com>
2023-09-07 17:31:25 +0530

    Time: 0 ms (100%), Space: 6 MB (29.23%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -25,3 +25,15 @@ public:
         return solve(x, (long)n);
     }
 };
+/*
+    Something to learn : 
+Integer contains 32 bits, 1 bit is used for a sign flag (0 is + and 1 is -) and remaining 31 bits for values.
+For negative values we can express 2^31 different values (from -1 to -2^31) and for positive values 
+it is one value less since we still need to express zero.
+So, for positive we can express 2^31-1values with 31 bits, from 1 to 2^31-1. 
+That's why max negative and max positive do not match.
+So, if n=-2147483648 and we multiply n*-1, 
+We will get a number that is greater than Integer.MAX_VALUE as integer range is from -2147483648 to 2147483647
+That is why, we convert it to long.
+Hope you understood.
+*/
